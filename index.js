74d690c2decd1ff2a73e83b0571bd4cc1ef9566e
Njagi Githaiga <njagigithaiga@gmail.com>
2024-03-28 02:52:42 +0300

    Submitting arithmetic_lab

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
+//Question 1:
 
-// Write your code here
+let multiply;
+const num1 = 2;
+const num2 = 31;
+multiply = num1*num2;
+console.log(multiply);
+
+
+//Question 2:
+
+let random = 10;
+
+console.log(Math.random(random));
+
+
+//Question 3:
+
+let mod;
+const num3 = 6;
+const num4 = 28;
+mod=num4%num3;
+
+console.log(mod);
+
+
+//Question 4:
+
+let max;
+const num5 = 3;
+const num6 = 8;
+const num7 = 13;
+const num8 = 20;
+max = (num5, num6, num7, num8);
+console.log(Math.max(max));
